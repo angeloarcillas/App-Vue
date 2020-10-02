4d1a6c23f9d49864173a5f3c0e90f3f25ae89ac0
angeloarcillas <angeloarcillas512@gmail.com>
2020-10-02 08:45:48 +0800

    First commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+      odio illum accusamus corporis dolor nam incidunt perspiciatis eius. Fugiat
+      quas enim aspernatur ipsa odit magni non, ducimus rerum aut. Qui.
+    </p>
   </div>
 </template>
