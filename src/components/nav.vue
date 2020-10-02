321a4b6a8cc2c2a6ff01ff8c811f1eb1e0742c60
angeloarcillas <angeloarcillas512@gmail.com>
2020-10-02 11:12:53 +0800

    Add tailwind & punch game & improve ui

---
+++ src/components/nav.vue
@@ -0,0 +1,15 @@
+
+<template>
+  <div class="pb-4">
+    <div class="relative z-10 bg-white p-4 pt-6 shadow">
+      <nav class="flex items-center">
+      <router-link class="mr-4 text-gray-700 hover:text-gray-900" to="/"
+        >Home</router-link
+      >
+      <router-link class="text-gray-700 hover:text-gray-900" to="/about"
+        >About</router-link
+      >
+      </nav>
+    </div>
+  </div>
+</template>
